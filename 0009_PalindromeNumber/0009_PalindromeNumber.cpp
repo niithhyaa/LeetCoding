af0211928a63fd01d336e6419572aa287f42d417
niithhyaa <143529113+niithhyaa@users.noreply.github.com>
2025-08-09 17:22:10 +0530

    Create 0009_PalindromeNumber.cpp

---
+++ 0009_PalindromeNumber/0009_PalindromeNumber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x==0) {return true;}
+        if ((x>0) && (x%10!=0)) {
+            long long rev = 0;
+            int y = x;
+            while (y!=0) {
+                int d = y%10;
+                rev = rev*10 + d;
+                y = y/10;
+            }
+            return rev == x;
+        }
+        else {
+            return false;
+        }
+    }
+};
