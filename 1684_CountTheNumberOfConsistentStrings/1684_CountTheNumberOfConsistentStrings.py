295bb417e168044ab7ee878f33db7d8bcabdfb01
Nithyashree M <143529113+niithhyaa@users.noreply.github.com>
2025-10-31 11:37:19 +0530

    Update 1684_CountTheNumberOfConsistentStrings.py

--- 1684_CountTheNumberOfConsistentStrings/1684_CountTheNumberOfConsistentStrings.py
+++ 1684_CountTheNumberOfConsistentStrings/1684_CountTheNumberOfConsistentStrings.py
@@ -1,11 +1,10 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
-        allowed_list = list(allowed)
         res = 0
         for i in range(len(words)):
             c=0
             for j in words[i]:
-                if j not in allowed_list:
+                if j not in allowed:
                     c+=1
                     break
             if c==0:
